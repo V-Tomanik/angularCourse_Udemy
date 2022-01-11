f442b2d77b0362b11f1261acc2d650c0b717355d
vv <victor.o.tomanik@gmail.com>
2022-01-10 23:42:57 -0300

    Adicionando header,recipelist,recipeitem e alterando os html

---
+++ src/app/recipes/recipes-detail/recipes-detail.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-detail',
+  template: `
+    <p>
+      recipes-detail works!
+    </p>
+  `,
+  styleUrls: ['./recipes-detail.component.css']
+})
+export class RecipesDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
