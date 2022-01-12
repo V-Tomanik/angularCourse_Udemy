a61ccbcdd3fe4a7993ba303cb67a49a43d7d834d
vv <victor.o.tomanik@gmail.com>
2022-01-11 23:50:31 -0300

    alterando a parte de shopping

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+	ingredients: Ingredient[] =[
+		new Ingredient('Maça',5),
+		new Ingredient('Tomate',8),
+	]; 
 
-  constructor() { }
+  	constructor() { }
 
-  ngOnInit(): void {
-  }
+  	ngOnInit(): void {
+  	}
 
 }
